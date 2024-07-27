23d250b741df648b5cae6dfd461dc4097d513fa6
Prem Kumar <premkumar5012002@gmail.com>
2024-07-27 14:42:16 +0530

    add texture render

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+int window_width = 800;
+int window_height = 600;
+
 bool is_running = false;
+uint32_t *color_buffer = NULL;
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
 
 // Initialize SDL Window and Renderer
 bool initialize_window(void)
@@ -18,8 +24,15 @@ bool initialize_window(void)
         return false;
     }
 
+    // Use SDL to query what is the fullscreen max. width and height
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+
     // Create a SDL Window at center of the screen with 800 x 600 resolution with always shown on top all application.
-    window = SDL_CreateWindow("Tiny3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_ALWAYS_ON_TOP);
+    window = SDL_CreateWindow("Tiny3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_ALWAYS_ON_TOP);
 
     if (window == NULL)
     {
@@ -36,10 +49,27 @@ bool initialize_window(void)
         return false;
     }
 
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     return true;
 }
 
-void setup(void) {}
+bool setup(void)
+{
+    // Allocate the required memory in bytes to hold the color buffer.
+    color_buffer = (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+
+    if (color_buffer == NULL)
+    {
+        fprintf(stderr, "Error allocating memory to color_buffer. \n");
+        return false;
+    }
+
+    // Creating a SDL texture that is used to display the color
+    color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
+
+    return true;
+}
 
 void process_input(void)
 {
@@ -68,23 +98,91 @@ void process_input(void)
 
 void update(void) {}
 
+void render_color_buffer(void)
+{
+    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)(sizeof(u_int32_t) * window_width));
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color)
+{
+    for (int y = 0; y < window_height; y++)
+    {
+        for (int x = 0; x < window_width; x++)
+        {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
+void draw_grid(void)
+{
+    for (int y = 0; y < window_height; y++)
+    {
+        for (int x = 0; x < window_width; x++)
+        {
+            if (x % 20 == 0 || y % 20 == 0)
+            {
+                color_buffer[(window_width * y) + x] = 0xFF333333;
+            }
+        }
+    }
+}
+
+void draw_dots(void)
+{
+    for (int y = 0; y < window_height; y = y + 20)
+    {
+        for (int x = 0; x < window_width; x = x + 20)
+        {
+            color_buffer[(window_width * y) + x] = 0xFF333333;
+        }
+    }
+}
+
+void draw_rect(int x, int y, int width, int height, uint32_t color)
+{
+    for (int _y = 0; _y < window_height; _y++)
+    {
+        for (int _x = 0; _x < window_width; _x++)
+        {
+            if (_y >= y && _y <= (y + height))
+            {
+                if (_x >= x && _x <= (x + width))
+                {
+                    color_buffer[(window_width * _y) + _x] = 0xFF333333;
+                }
+            }
+        }
+    }
+}
+
 void render(void)
 {
-    // Set the drawing color
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    draw_rect(100, 100, 200, 200, 0xFFFFFF);
 
-    // Clears or fills the background with the current drawing color set via SDL_SetRenderDrawColor.
-    SDL_RenderClear(renderer);
+    render_color_buffer();
+
+    // Clearing the color buffer with specified color before next rendering.
+    clear_color_buffer(0xFF000000);
 
     // Present the rendered content to the screen.
     SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void)
+{
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main(int argc, char *argv[])
 {
     is_running = initialize_window();
 
-    setup();
+    is_running = setup();
 
     // Game loop
     while (is_running)
@@ -93,6 +191,8 @@ int main(int argc, char *argv[])
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
