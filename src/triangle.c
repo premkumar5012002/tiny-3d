7e05fc8c5484032e10225e0f0fbe1e0a50329bf3
Prem Kumar <premkumar5012002@gmail.com>
2024-08-14 19:38:55 +0530

    impl draw filled triangle

--- src/triangle.c
+++ src/triangle.c
@@ -1,1 +1,35 @@
 #include "triangle.h"
+
+void int_swap(int* a, int* b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
+                          int32_t color) {
+  // We need to sort the vectices by y-coordinate ascending (y0 < y1 < y2)
+  if (y0 > y1) {
+    int_swap(&y0, &y1);
+    int_swap(&x0, &x1);
+  }
+
+  if (y1 > y2) {
+    int_swap(&y1, &y2);
+    int_swap(&x1, &x2);
+  }
+
+  if (y0 > y1) {
+    int_swap(&y0, &y1);
+    int_swap(&x0, &x1);
+  }
+
+  int My = y1;
+  int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+
+  // Draw flat-bottom triangle
+  fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My);
+
+  // Draw flat-top triangle
+  fill_top_bottom_triangle(x1, y1, Mx, My, x2, y2);
+}
--- src/vector.h
+++ src/vector.h
@@ -12,23 +12,18 @@ vec2_t vec2_add(vec2_t v1, vec2_t v2);
 vec2_t vec2_sub(vec2_t v1, vec2_t v2);
 vec2_t vec2_mul(vec2_t v, float factor);
 vec2_t vec2_div(vec2_t v, float factor);
-
 float vec2_length(vec2_t* v);
 void vec2_normalize(vec2_t* v);
-
 float vec2_dot(vec2_t v1, vec2_t v2);
 
 vec3_t vec3_add(vec3_t v1, vec3_t v2);
 vec3_t vec3_sub(vec3_t v1, vec3_t v2);
 vec3_t vec3_mul(vec3_t v, float factor);
 vec3_t vec3_div(vec3_t v, float factor);
-
-float vec3_length(vec3_t* v);
-void vec3_normalize(vec3_t* v);
-
-float vec3_dot(vec3_t v1, vec3_t v2);
-vec3_t vec3_cross(vec3_t v1, vec3_t v2);
-
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
+float vec3_length(vec3_t* v);
+void vec3_normalize(vec3_t* v);
+float vec3_dot(vec3_t v1, vec3_t v2);
+vec3_t vec3_cross(vec3_t v1, vec3_t v2);
